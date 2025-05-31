2071cb6112bf38ff7d5a5d4a072a9aa46c04ad36
xiuxiugod <126649125+xiuxiugod@users.noreply.github.com>
2025-05-31 12:49:17 +0800

    Update Source.cpp

--- 2048.2048/Source.cpp
+++ 2048.2048/Source.cpp
@@ -1,12 +1,11 @@
 #include "Game.h"
-#include <cstdlib> // For srand, exit
-#include <ctime>   // For time
+#include <cstdlib>
+#include <ctime> 
 
 int main() {
-    srand(static_cast<unsigned int>(time(0))); // 僅在 main 函數中設置一次亂數種子
-
-    Game game; // 直接創建 Game 物件，Produce 物件在 Game 內部創建並初始化
-    game.run(); // 啟動遊戲主循環
+    srand(static_cast<unsigned int>(time(0))); 
+    Game game; 
+    game.run(); 
 
     return 0;
 }
