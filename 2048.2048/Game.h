353086407c7d10776f501e725acb582c682bd37b
xiuxiugod <126649125+xiuxiugod@users.noreply.github.com>
2025-05-31 12:47:46 +0800

    Update Game.h

--- 2048.2048/Game.h
+++ 2048.2048/Game.h
@@ -1,20 +1,20 @@
 #ifndef GAME_H
 #define GAME_H
-#include<iostream>
-#include"Produce.h"
 
+#include <iostream>
+#include <cstdlib> // For exit()
+#include "Produce.h"
+
+// 跨平台輸入和清屏宏定義
 #ifdef _WIN32
 #include <conio.h>
+#define GETCH _getch
+#define CLRSCR system("cls")
 #else
-#include <termios.h>
-#include <unistd.h>
-#include <cstdio>
-#endif
-
-using namespace std;
-
-#ifndef _WIN32
-int getch_linux() {
+#include <termios.h> // For termios functions
+#include <unistd.h>  // For STDIN_FILENO
+#include <cstdio>    // For getchar
+int custom_getch() {
     struct termios oldt, newt;
     int ch;
     tcgetattr(STDIN_FILENO, &oldt);
@@ -25,31 +25,30 @@ int getch_linux() {
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
 }
-#define GETCH getch_linux
+#define GETCH custom_getch
 #define CLRSCR system("clear")
-#else
-#define GETCH _getch
-#define CLRSCR system("cls")
 #endif
 
-class Game{
+class Game {
 private:
-	Produce A;
-	void shiftLeft();
-	void mergeLeft();
-	void moveLeft();
-	void shiftRight();
-	void mergeRight();
-	void moveRight();
-	void shiftUp();
-	void mergeUp();
-	void moveUp();
-	void shiftDown();
-	void mergeDown();
-	void moveDown();
+    Produce A; // Game 類內部持有 Produce 對象
+    void shiftLeft();
+    void mergeLeft();
+    void moveLeft();
+    void shiftRight();
+    void mergeRight();
+    void moveRight();
+    void shiftUp();
+    void mergeUp();
+    void moveUp();
+    void shiftDown();
+    void mergeDown();
+    void moveDown();
+
 public:
-	Game();
-	void run();
-	bool isGameOver();
+    Game(); // 無參數建構函數
+    void run(); // 替代原來的 play() 函數，作為遊戲主循環
+    bool isGameOver();
 };
+
 #endif
