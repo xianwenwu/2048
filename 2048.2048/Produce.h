e82e133c6097f205f81611937e37721b4d17808c
xiuxiugod <126649125+xiuxiugod@users.noreply.github.com>
2025-05-31 12:48:26 +0800

    Update Produce.h

--- 2048.2048/Produce.h
+++ 2048.2048/Produce.h
@@ -1,21 +1,21 @@
 #ifndef R_H
 #define R_H
-#include<iostream>
-#include<iomanip>
-#include<cstdlib>
-#include<ctime>
-#include <vector>
-#include <algorithm>
-#include <random> // For std::mt19937
 
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <cstdlib> // For rand()
+#include <ctime>   // For time()
+#include <vector>  // For std::vector
+#include <algorithm> // For std::shuffle
+#include <random>    // For std::mt19937
 
-class Produce
-{
+class Produce {
 public:
-	int board[4][4];
-	Produce();
-	void generate_random_tile();
-	void print_board();
+    int board[4][4];
+    Produce(); // 初始化遊戲面板
+    void generate_random_tile_one_or_two(); // 隨機產生 1 或 2 個方塊
+    void generate_random_tile(int count); // 產生指定數量的方塊
+    void print_board(); // 印出初始畫面、更新後畫面
 };
+
 #endif
