580422d7fe80c024e1e7c96d41423a100b8e5d62
xiuxiugod <126649125+xiuxiugod@users.noreply.github.com>
2025-05-31 12:56:37 +0800

    Update Source.cpp

--- 2048.2048/Source.cpp
+++ 2048.2048/Source.cpp
@@ -1,11 +1,10 @@
-#include "Game.h"
-#include <cstdlib>
-#include <ctime> 
+#include"Game.h"
+#include<ctime>
+#include<cstdlib>
 
 int main() {
-    srand(static_cast<unsigned int>(time(0))); 
-    Game game; 
-    game.run(); 
-
-    return 0;
+	srand(static_cast<unsigned int>(time(0)));
+	Produce p;
+	Game play(p);
+	play.play();
 }
