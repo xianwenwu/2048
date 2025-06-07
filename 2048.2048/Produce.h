2307cca143bc4ea2a6df7eceb4b77a6151ddafe3
xiuxiugod <126649125+xiuxiugod@users.noreply.github.com>
2025-06-07 20:12:35 +0800

    Update Produce.h

--- 2048.2048/Produce.h
+++ 2048.2048/Produce.h
@@ -4,6 +4,8 @@
 #include<iomanip>
 #include<cstdlib>
 #include<ctime>
+#include<string>
+#include<windows.h>
 using namespace std;
 
 class Produce
@@ -12,6 +14,6 @@ public:
 	int board[4][4];
 	Produce();
 	void generate_random_tile();
-	void print_board();
+	void print_board(HANDLE hConsole);
 };
 #endif
