4aa5a7f8021dad6ac1714f669e0fb49ac07124ce
xiuxiugod <126649125+xiuxiugod@users.noreply.github.com>
2025-06-07 19:58:58 +0800

    Update Game.h

--- 2048.2048/Game.h
+++ 2048.2048/Game.h
@@ -1,28 +1,38 @@
 #ifndef GAME_H
 #define GAME_H
-#include<iostream>
+#include <iostream>
 #include <conio.h>
-#include"Produce.h"
+#include <string>
+#include <chrono>
+#include "Produce.h"
 using namespace std;
 
-class Game{
+class Game {
 private:
-	void shiftLeft();
-	void mergeLeft();
-	void moveLeft();
-	void shiftRight();
-	void mergeRight();
-	void moveRight();
-	void shiftUp();
-	void mergeUp();
-	void moveUp();
-	void shiftDown();
-	void mergeDown();
-	void moveDown();
+    enum GameMode { ENDLESS, CHALLENGE };
+    GameMode mode;
+    string nickname;
+    chrono::steady_clock::time_point startTime;
+    bool winConditionMet;
+
+    void shiftLeft();
+    void mergeLeft();
+    void moveLeft();
+    void shiftRight();
+    void mergeRight();
+    void moveRight();
+    void shiftUp();
+    void mergeUp();
+    void moveUp();
+    void shiftDown();
+    void mergeDown();
+    void moveDown();
+    bool checkWinCondition();
+
 public:
-	Produce A;
-	Game(Produce);
-	void play();
-	bool isGameOver();
+    Produce A;
+    Game(Produce);
+    void play();
+    bool isGameOver();
 };
 #endif
